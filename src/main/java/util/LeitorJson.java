ba6035949527a294c4a052cb5d4d934babeecf49
Cesar Silva <84938508+cfsilva28@users.noreply.github.com>
2021-07-27 22:31:28 -0300

    WriterJson

--- src/main/java/util/LeitorJson.java
+++ src/main/java/util/LeitorJson.java
@@ -2,6 +2,8 @@ package util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
@@ -12,7 +14,24 @@ public class LeitorJson {
 	static Gson gson = new Gson(); // biblioteca do google para manipular arquivos Json
 	static Reader reader;
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
+		gravaArquivoJson();
+		lerArquivoJson();
+
+	}
+
+	public static void gravaArquivoJson() throws IOException {
+		mapJson.put("url", "http://www.google.com.br");
+		mapJson.put("curso", "testes automatizados api");
+		mapJson.put("endereço", "carlos lacerda, 57 ");
+
+		FileWriter writer = new FileWriter("./src/main/resources/MassaTeste_writer.json");
+		gson.toJson(mapJson, writer);
+		writer.flush();
+
+	}
+
+	public static void lerArquivoJson() throws FileNotFoundException {
 		reader = new FileReader("./src/main/resources/MassaTeste.json");
 		mapJson = gson.fromJson(reader, Map.class);
 		System.out.println(mapJson.get("nome"));
